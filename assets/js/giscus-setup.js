7916a09b9ad3794dcb96dd1547907a86c25fc04f
gregaiosa <geester98@gmail.com>
2025-12-18 22:41:51 +0000

    Deploying to gh-pages from @ gregaiosa/gregaiosa.github.io@079bd1348be554878e6b1677d7b8ab4d4e23fd50 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "gregaiosa/gaiosa-bot.github.io",
+    "data-repo": "gregaiosa/gregaiosa.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
