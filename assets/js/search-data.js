b4f02af49d2375be8f19267c07fb64aa0379d4ff
gregaiosa <geester98@gmail.com>
2025-12-22 15:17:55 +0000

    Deploying to gh-pages from @ gregaiosa/gregaiosa.github.io@393df36d85a8a32e22aed274f5a69a884e69a31b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,13 +11,6 @@ ninja.data = [{
     },
   },{id: "nav-resume",
           title: "Resume",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/resume/";
-          },
-        },{id: "nav-resume",
-          title: "Resume",
           description: "",
           section: "Navigation",
           handler: () => {
